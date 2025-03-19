6d4689de5e4013be165c4b6da745d3028359daa7
CodeAxeAttacks <iambatmanov@gmail.com>
2025-03-19 13:55:30 +0300

    Template ready

---
+++ src/test/java/com/wot/base/BaseTest.java
@@ -0,0 +1,33 @@
+package com.wot.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @Parameters("browser")
+    @BeforeClass
+    public void setup(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
